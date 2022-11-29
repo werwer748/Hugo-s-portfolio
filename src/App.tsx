4ade071f4877f2b0ca9be464f9d38868daa89e72
werwer748 <werwer748@naver.com>
2022-11-29 17:01:10 +0900

    초반세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const App = () => {
+    return (
+        <>
+            <div style={{ color: "black", width: "100%", height: '50px', backgroundColor: 'red' }}>hahahahahahahaah</div>
+        </>
+    );
+};
+
+export default App;
